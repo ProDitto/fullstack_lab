5f39de79c7092b1e8f36a083e4344db6020ff524
Ditto <159179584+ProDitto@users.noreply.github.com>
2025-09-10 03:48:24 +0000

    implement react.ts frontend with tailwind css and themes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="bg-background-primary text-text-primary h-screen w-screen dark">
+
+      <h1 className="text-3xl font-bold underline">
+        Hello worldd!
+      </h1>
+    </div>
+  )
+}
